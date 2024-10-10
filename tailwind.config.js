24256af2b6b096de962cc1c269ff1e7a4a34316a
Kashif <kashif.riaz7272@gmail.com>
2024-10-10 10:32:10 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+
+        'customgrey': '#666',
+        'customblack': '#111'
+    },
+    fontFamily: {
+
+      'customfont': ['Poppins', 'serif'],
+  },
+  },
+  plugins: [],
+}
+}
